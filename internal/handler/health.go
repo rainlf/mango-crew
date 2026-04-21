04b330e5251e76a3d84b62d4c1340cb7e9df23bc
heyu <heyu.zero@bytedance.com>
2026-04-21 14:56:48 +0800

    update

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -1,14 +1,13 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/rainlf/mango-crew/pkg/response"
 )
 
 // RegisterHealthRoutes 注册健康检查路由
 func RegisterHealthRoutes(r *gin.RouterGroup) {
 	r.GET("/health", func(c *gin.Context) {
-		c.String(http.StatusOK, "OK")
+		response.Success(c, "ok")
 	})
 }
